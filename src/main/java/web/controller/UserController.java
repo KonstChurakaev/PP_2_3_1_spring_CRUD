56dba9102869d1e0c3023c7fc64d762da841f57a
KonstChurakaev <111491525+KonstChurakaev@users.noreply.github.com>
2022-11-04 14:37:12 +0500

    Исправлены названия методов

--- src/main/java/web/controller/UserController.java
+++ src/main/java/web/controller/UserController.java
@@ -3,12 +3,9 @@ package web.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import web.model.User;
 import web.service.UserService;
-
-import javax.validation.Valid;
 
 @Controller
 public class UserController {
@@ -20,32 +17,27 @@ public class UserController {
         this.userService = userService;
     }
 
-    @GetMapping("/")
-    public String home() {
-        return "redirect:users";
-    }
-
     @GetMapping("/users")
-    public String getAllUsers(Model model) {
+    public String getListAllUsers(Model model) {
         model.addAttribute("users", userService.getAllUser());
         return "/users";
     }
 
     @GetMapping("/add")
-    public String addUser(Model model) {
+    public String goPageForAddNewUser(Model model) {
         User user = new User();
         model.addAttribute("user", user);
         return "addUser";
     }
 
     @PostMapping("/input")
-    public String inputUser(@ModelAttribute("user") User user) {
+    public String saveNewUser(@ModelAttribute("user") User user) {
         userService.addUser(user);
         return "redirect:users";
     }
 
     @GetMapping("/{id}/edit")
-    public String getUserById(@PathVariable("id") Long id, Model model) {
+    public String goPageEditUser(@PathVariable("id") Long id, Model model) {
         model.addAttribute("user", userService.getUserById(id));
         return "updateUser";
     }
@@ -57,10 +49,8 @@ public class UserController {
     }
 
     @DeleteMapping("/{id}")
-    public String delete(@PathVariable("id") Long id) {
+    public String deleteUser(@PathVariable("id") Long id) {
         userService.removeUserById(id);
         return "redirect:users";
     }
-
-
 }
